27828b1c5e8f6a80fdaf2123b7eae9eeac3910a0
user <user@DESKTOP-HFVGVI8>
2020-05-09 19:52:14 +0600

    this is my first git hub project

---
+++ Hiworld/src/com/pooja/FirstProgram.java
@@ -0,0 +1,12 @@
+package com.pooja;
+
+public class FirstProgram {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("hi world");
+
+	}
+
+}
