0272381c6e0365d97ac040dbd79a4a81337e644a
user <user@DESKTOP-HFVGVI8>
2020-05-09 20:13:16 +0600

    this is second code

---
+++ Hiworld1/src/com/pooja/Secondcode1.java
@@ -0,0 +1,14 @@
+package com.pooja;
+
+public class Secondcode1 {
+
+	public static void main(String[] args) 
+	{// TODO Auto-generated method stub
+		System.out.println("second project1");
+
+		// TODO Auto-generated method stub
+				System.out.println("second project");
+
+	}
+
+}
